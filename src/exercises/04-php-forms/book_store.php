fdf05360d6feefccef42af92dc060f2b74eda8e6
Finn OBrien <n00255435@iadt.ie>
2026-02-10 13:20:46 +0000

    commit pls

--- src/exercises/04-php-forms/book_store.php
+++ src/exercises/04-php-forms/book_store.php
@@ -24,6 +24,8 @@ $rrors = [];
 // Start the session
 startSession();
 
+
+
 try {
     // =========================================================================
     // STEP 1: View Posted Data
@@ -60,6 +62,7 @@ try {
         'isbn' => $_POST['isbn'] ?? null,
         'format_ids' => $_POST['format_ids'] ?? [],
         'description' => $_POST['description'] ?? null,
+        'cover' => $_FILES['cover'] ?? null,
     ];
 
 
@@ -82,7 +85,8 @@ try {
         'year' => "required|noempty|minvalue:1900|maxvalue:" .$year,
         'isbn' => "required|noempty|min:13|max:13",
         'format_ids' => "required|noempty|array|min:1|max:4",
-        'description' => "required|noempty|min:10"
+        'description' => "required|noempty|min:10",
+        'cover' => 'required|file|image|mimes:jpeg,jpeg,png|max_file_size:5242880'
     ];
     $validator = new Validator ($data, $rules);
 
@@ -92,6 +96,9 @@ try {
         }
         throw new Exception('Validation failed');
     }
+
+    $uploader = new ImageUpload();
+    $imageFilename = $uploader->process($_FILES['cover']);
 
     echo "Validation Successful!";
 
