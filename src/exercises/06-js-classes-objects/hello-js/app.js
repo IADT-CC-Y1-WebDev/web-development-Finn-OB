2288f5f0ee772372ab2736fb31914d64917d2ad6
Finn OBrien <n00255435@iadt.ie>
2026-03-02 12:57:31 +0000

    commit

---
+++ src/exercises/06-js-classes-objects/hello-js/app.js
@@ -0,0 +1,33 @@
+console.log("Hello World");
+
+function timesTwo(inputNmber){
+    console.log(inputNumber * 2);
+}
+
+timesTwo(1);
+
+let user = {
+    firstName: "John",
+    lastName: "Jones",
+    age: 32,
+    hobbies: ["Gym", "Movies"],
+    friends:[
+        {
+        firstName: "Tim",
+        lastName: "Murphy",
+        age: 25, 
+        } 
+
+        {
+        firstName: "Jake",
+        lastName: "Walsh",
+        age: 28, 
+        } 
+    ]
+};
+
+console.log(user.friends.lastname[1]);
+
+let donuts = ["Chocolate", "Jam", "Custard"];
+
+donuts.
